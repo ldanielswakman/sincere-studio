de87fc9292e9ccd4c55465b071e322b5249695c3
Daniel Swakman <dswakman@oefenweb.nl>
2015-05-18 19:21:10 +0300

    added user

---
+++ site/accounts/daniel.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: daniel
+email: hello@ldaniel.eu
+password: >
+  $2a$10$I9HlAK7.cRlogGDXANRqSed7JLlRlI2vzjw/IFNikRnwkcYk5YnyO
+language: en
+role: admin
+token: 387ffdb27245ede019dc518091ef333426bd936e
+history:
+  - projects
+  - home
+  - null
