cbca619af0d30f3fe4642ce766257e54813f29ff
L Daniel Swakman <d.swakman@gmail.com>
2017-05-04 14:50:13 +0200

    updated gitignore

---
+++ site/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+---------------------------------------
+Kirby Configuration
+---------------------------------------
+
+By default you don't have to configure anything to
+make Kirby work. For more fine-grained configuration
+of the system, please check out http://getkirby.com/docs/advanced/options
+
+*/
