786d1313fb3a7e5809521268c3eb3d7d714b02c0
L Daniel Swakman <d.swakman@gmail.com>
2017-05-09 16:30:53 +0200

    added flexboxgrid& owl carousel

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -15,21 +15,27 @@
   $local = strpos($_SERVER['SERVER_NAME'], 'localhost');
   if($local === false) :
     // Bootstrap
-    echo css('//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css');
+    echo css('//cdn.jsdelivr.net/flexboxgrid/6.3.0/flexboxgrid.min.css');
     // Ionicons
     echo css('http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css');
+    // Owl Carousel
+    echo css('assets/css/owl.carousel.min.css');
     // Google Fonts
     echo css('https://fonts.googleapis.com/css?family=Overpass:400,400i,700,700i');
-    // JQuery SmoothScroll
+    // jQuery
     echo js('http://code.jquery.com/jquery-1.11.1.min.js');
+    // jQuery SmoothScroll
     echo js('http://cdnjs.cloudflare.com/ajax/libs/jquery-smooth-scroll/1.5.4/jquery.smooth-scroll.min.js');
+    // Owl Carousel
+    echo js('https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js');
   // if localhost then load local assets
   else :
-    // echo css('assets/css/bootstrap.min.css');
+    echo css('assets/css/flexboxgrid.min.css');
     echo css('assets/css/ionicons.min.css');
+    echo css('assets/css/owl.carousel.min.css');
     echo js('assets/js/jquery-1.11.1.min.js');
     echo js('assets/js/jquery.smooth-scroll.min.js');
-    // echo js('assets/js/fastclick.min.js');
+    echo js('assets/js/owl.carousel.min.js');
   endif;
 
   // assets
