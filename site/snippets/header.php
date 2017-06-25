08ecdbef5f83adcabb43d300a4c191e02b25c166
L Daniel Swakman <d.swakman@gmail.com>
2017-06-25 09:45:47 +0200

    correct min js/css

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,11 +14,11 @@
   $css_assets = (c::get('env') !== 'DEV') ? array(
     // '//cdn.jsdelivr.net/flexboxgrid/6.3.0/flexboxgrid.min.css',
     // '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css',
-    'assets/css/style.css',
+    'assets/css/style.min.css',
   ) : array(
     // 'assets/css/flexboxgrid.min.css',
     // 'assets/css/owl.carousel.min.css',
-    'assets/css/style.min.css',
+    'assets/css/style.css',
   );
 
   // checks if not on localhost, then serves assets from CDN
@@ -26,12 +26,12 @@
     '//code.jquery.com/jquery-1.11.1.min.js',
     // '//cdnjs.cloudflare.com/ajax/libs/jquery-smooth-scroll/1.5.4/jquery.smooth-scroll.min.js',
     // '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js',
-    'assets/js/scripts.js',
+    'assets/js/scripts.min.js',
   ) : array(
     'assets/js/vendor/jquery-1.11.1.min.js',
     // 'assets/js/vendor/jquery.smooth-scroll.min.js',
     // 'assets/js/vendor/owl.carousel.min.js',
-    'assets/js/scripts.min.js',
+    'assets/js/scripts.js',
   );
 
   echo css($css_assets);
