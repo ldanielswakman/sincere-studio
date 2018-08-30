5a559d6a4d0093e54d13cc6e9407ef43942fbb37
L Daniel Swakman <d.swakman@gmail.com>
2018-08-30 12:02:32 +0200

    meta tags more relevant/descriptive

--- site/snippets/header-metadata.php
+++ site/snippets/header-metadata.php
@@ -1,12 +1,42 @@
+<?
+
+// Set title
+$title = r($page->isHomePage(), $site->title()->html(), $page->title()->html() . ' — ' . $site->title()->html());
+$site_title = $site->title()->html();
+
+// Set description
+$descr = $site->description()->html();
+if($page->description()->isNotEmpty()) {
+	$descr = excerpt($page->description(), 30, 'words');
+} else if($page->text()->isNotEmpty()) {
+	$descr = excerpt($page->text(), 30, 'words');
+} else if($page->intro()->isNotEmpty()) {
+	$descr = excerpt($page->intro(), 30, 'words');
+}
+
+// Set image
+$image_url = r($site->meta_image()->isNotEmpty(), $site->image($site->meta_image())->url());
+if($page->featuredimage()->isNotEmpty()) {
+	$image_url = $page->image($page->featuredimage())->url();
+}
+?>
+
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-<meta name="description" content="<?= $site->description()->html() ?>">
+<meta name="description" content="<?= $descr ?>">
 <meta name="keywords" content="<?= $site->keywords()->html() ?>">
 <meta name="author" content="<?php echo $site->author()->html() ?>" />
 
 <!-- Social share parameters -->
-<meta property="og:image" content="<?= $site->image($site->meta_image())->url() ?>" />
-<meta property="og:title" content="<?= $page->title() ?>" />
+<meta property="og:image" content="<?= $image_url ?>" />
+<meta property="og:title" content="<?= $title ?>" />
 <meta property="og:site_name" content="<?= $site->title() ?>" />
-<meta property="og:description" content="<?= $site->description()->html() ?>" />
+<meta property="og:description" content="<?= $descr ?>" />
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@ldanielswakman">
+<meta name="twitter:title" content="L Daniel Swakman">
+<meta name="twitter:description" content="<?= $descr ?>">
+<meta name="twitter:image" content="<?= $image_url ?>">
+
