ffac822163fb1f064e2daebb5b1e120cc360e889
ldanielswakman <d.swakman@gmail.com>
2019-05-12 12:20:59 +0200

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -6,7 +6,7 @@
 
       <section>
         <div class="row">
-          <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-7 col-md-offset-3">
+          <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             <?= $page->text()->kirbytext() ?>
           </div>
         </div>
