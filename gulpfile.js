10e705e1b81343b2cc8e931a66b19330a2d0dd51
ldanielswakman <d.swakman@gmail.com>
2020-01-04 13:56:58 +0100

    Update pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -8,47 +8,46 @@ var cleanCSS = require('gulp-clean-css');
 // JS compiling
 var concat = require('gulp-concat');
 var minify = require('gulp-minify');
+// Browsersync
+var browserSync = require('browser-sync').create();
 
-// Concatenate Sass task
-// gulp.src('assets/scss/**/*.scss')
-gulp.task('sass', function() {
-  gulp.src('assets/scss/style.scss')
+function style() {
+  return gulp.src('assets/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css/'));
-});
-
-// Clean & minify CSS (after Sass)
-gulp.task('clean_css', ['sass'], function() {
-  gulp.src('assets/css/style.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css/'))
+    .pipe(browserSync.stream())
+    .pipe(cleanCSS({compatibility: 'ie9', debug: true}))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./assets/css/'));
-});
-
-// Combine style tasks
-gulp.task('styles', ['sass', 'clean_css']);
+    .pipe(gulp.dest('assets/css'));
+}
 
 // Concatenate & minify JS
-gulp.task('scripts', function() {
+function scripts() {
   return gulp.src([
-    // './assets/js/vendor/jquery-1.11.1.min.js',
     './assets/js/vendor/jquery.smooth-scroll.min.js',
     './assets/js/vendor/owl.carousel.min.js',
     './assets/js/vendor/twitterfetcher.min.js',
     './assets/js/vendor/autosize.min.js',
     './assets/js/components/*.js'
-    ])
-    .pipe(concat('scripts.js'))
-    .pipe(minify({ ext:{
-            src:'.js',
-            min:'.min.js'
-        },
-    }))
-    .pipe(gulp.dest('./assets/js/'));
-});
+  ])
+  .pipe(concat('scripts.js'))
+  .pipe(minify({ ext:{
+      src:'.js',
+      min:'.min.js'
+    },
+  }))
+  .pipe(gulp.dest('./assets/js'));
+}
 
-// Watch task
-gulp.task('default',function() {
-    gulp.watch('assets/scss/**/*.scss',['styles']);
-    gulp.watch('assets/js/components/**/*.js',['scripts']);
-});
+function watch() {
+  browserSync.init({
+    proxy: 'http://localhost/ldaniel-site',
+    open: false
+  });
+  gulp.watch('assets/scss/**/*.scss', style);
+  gulp.watch('assets/js/components/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.scripts = scripts;
+exports.default = watch;
