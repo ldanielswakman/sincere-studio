f8389d710091f45f9e408d5cbd2352c94c5e7e07
L Daniel Swakman <d.swakman@gmail.com>
2021-01-04 21:42:43 +0100

    project load animations + content

--- site/snippets/project-info.php
+++ site/snippets/project-info.php
@@ -1,13 +1,13 @@
 <section class="block block--info">
   <div class="row">
-    <div class="col-xs-12 col-sm-7 col-md-5 col-md-offset-2">
+    <div class="col-xs-12 col-sm-7 col-md-5 col-md-offset-2 project-info-main">
 
       <h1><?= $page->title()->html() ?></h1>
 
       <blockquote><?= $page->intro()->or($page->description())->kt() ?></blockquote>
 
       <div class="actions">
-        <a href="#block_1" target="_blank">Dive in</a>
+        <a href="#block_1" target="_blank">Read on</a>
 
         <?php if($page->projecturl()->isNotEmpty()): ?>
           <a href="<?= $page->projecturl() ?>" target="_blank">See it live</a>
@@ -17,9 +17,10 @@
     </div>
     <div class="col-xs-12 col-sm-4 col-sm-offset-1 col-md-3 col-md-offset-1">
 
-      <div class="project-info">
+      <div class="project-info-side">
 
         <div class="disciplines">
+          <!-- <h4>DISCIPLINES</h4> -->
           <?php foreach(explode(',', $page->tags()->html()) as $tag) : ?>
             <div class="discipline">
               <a href="<?= $page->parent()->url() . '/tag:' . $tag ?>">
@@ -32,6 +33,7 @@
         <div class="year"><?= $page->year() ?></div>
 
         <div class="team">
+          <!-- <h4>TEAM</h4> -->
           <?php foreach($page->team()->toStructure() as $member) : ?>
             <div class="member">
               <a href="<?= $member->link() ?>" target="_blank">
