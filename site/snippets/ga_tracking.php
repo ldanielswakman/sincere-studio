8e2ecb86a8a4d1b5554c89e682d40c7499181ee9
L Daniel Swakman <d.swakman@gmail.com>
2021-01-06 22:52:22 +0100

    Update ga_tracking.php

--- site/snippets/ga_tracking.php
+++ site/snippets/ga_tracking.php
@@ -1,9 +1,9 @@
-<!-- google analytics tracking code -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162851749-1"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GDL19YQV2K"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-162851749-1');
+  gtag('config', 'G-GDL19YQV2K');
 </script>
