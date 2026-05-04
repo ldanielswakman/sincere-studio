472524e135216658a7c938a231f9302f4c0693f9
L Daniel Swakman <d.swakman@gmail.com>
2026-05-04 14:30:12 +0200

    Update head.php
    Added inclusive sans font import

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -27,4 +27,8 @@
   <link rel="apple-touch-icon" href="<?= url('assets/images/app-icon.png') ?>">
   <meta name="theme-color" content="#ffffff">
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+
 </head>
